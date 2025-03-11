b29db02a08dfd73812d81ebacc8b815f8153327f
Frankie <frankie.m.18@hotmail.com>
2025-03-10 22:36:14 -0700

    attached to backend, salmon list working

--- src/components/SalmonDashboard/SalmonDashboard.jsx
+++ src/components/SalmonDashboard/SalmonDashboard.jsx
@@ -1,24 +1,80 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
 import "./SalmonDashboard.scss";
-import coho from "../../assets/images/coho.png";
 
 function SalmonDashboard() {
+  const baseURL = import.meta.env.VITE_API_URL;
+  const [isLoading, setIsLoading] = useState(true);
+  const [salmonData, setSalmonData] = useState([]);
+
+  useEffect(() => {
+    async function getSalmon() {
+      try {
+        const response = await axios.get(`${baseURL}/salmon`);
+        setSalmonData(response.data);
+        console.log(response.data);
+      } catch (error) {
+        console.error("Error fetching salmon:", error);
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    getSalmon();
+  }, []);
+  if (isLoading) {
+    return <p>Loading numbers...</p>;
+  }
   return (
     <div className="salmon">
       <ul className="salmon__list">
-        <li className="salmon__item">
-          Coho <img src={coho} alt="coho salmon" className="salmon__image" />
-          <ul className="salmon__details-list">
-            <li className="salmon__details-item">Latin Name</li>
-            <li className="salmon__details-item">Other common names</li>
-            <li className="salmon__details-item">Average weight</li>
-            <li className="salmon__details-item">Average Size</li>
-            <li className="salmon__details-item">Life cycle</li>
-          </ul>
-        </li>
-        <li className="salmon__item">Chinook</li>
-        <li className="salmon__item">Pink</li>
-        <li className="salmon__item">Sockeye</li>
-        <li className="salmon__item">Chum</li>
+        {salmonData.map((salmon) => (
+          <li key={salmon.id} className="salmon__item">
+            {salmon.name}{" "}
+            <img
+              src={`${baseURL}${salmon.image}`}
+              alt="coho salmon"
+              className="salmon__image"
+            />
+            <ul className="salmon__details-list">
+              <li className="salmon__details-item">
+                Latin Name
+                <h6 className="salmon__text">{salmon.latinName}</h6>
+              </li>
+              <li className="salmon__details-item">
+                Other common names
+                <h6 className="salmon__text">{salmon.otherNames}</h6>
+              </li>
+              <li className="salmon__details-item">
+                Average weight
+                <h6 className="salmon__text">{salmon.averageWeight}</h6>
+              </li>
+              <li className="salmon__details-item">
+                Average Size
+                <h6 className="salmon__text">{salmon.averageSize}</h6>
+              </li>
+              <li className="salmon__details-item">
+                Life cycle
+                <h6 className="salmon__text">{salmon.lifeCycle}</h6>
+              </li>
+              <li className="salmon__details-item">
+                Did you know...
+                <h6 className="salmon__text">{salmon.funFact1}</h6>
+              </li>
+              <li className="salmon__details-item">
+                Did you know...
+                <h6 className="salmon__text">{salmon.funFact2}</h6>
+              </li>
+              <li className="salmon__details-item">
+                Did you know...
+                <h6 className="salmon__text">{salmon.funFact3}</h6>
+              </li>
+              <li className="salmon__details-item">
+                Did you know...
+                <h6 className="salmon__text">{salmon.funFact4}</h6>
+              </li>
+            </ul>
+          </li>
+        ))}
       </ul>
     </div>
   );
